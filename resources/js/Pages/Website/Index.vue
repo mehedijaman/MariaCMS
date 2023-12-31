4f55a5c53a0bf8d09438a51372cd5bb2cebdb4fe
Mehedi Jaman <mail4mjaman@gmail.com>
2024-01-01 00:46:31 +0600

    - Homepage style modified

--- resources/js/Pages/Website/Index.vue
+++ resources/js/Pages/Website/Index.vue
@@ -4,6 +4,8 @@ import PrimaryButton from "@/Components/PrimaryButton.vue";
 import SecondaryButton from "@/Components/SecondaryButton.vue";
 import Hero from './Hero.vue';
 import Slider from './Slider.vue';
+import FAQ from "./FAQ.vue";
+import Testimonials from "./Testimonials.vue";
 
 const props = defineProps({
     title: String,
@@ -14,6 +16,14 @@ const props = defineProps({
         <Slider></Slider>
         <section class="min-h-[calc(100vh)] bg-pattern grid place-items-center bg-pattern" id="home">
             <Hero></Hero>
+            <div class="max-w-7xl mx-auto w-full">
+
+                <div class="max-w-3xl">
+                    <FAQ></FAQ>
+                </div>
+                <Testimonials></Testimonials>
+            </div>
+
             <!-- <div data-aos="fade-up" data-aos-duration="1000"
                 class="grid grid-cols-1 text-center gap-4 py-8 max-w-7xl m-auto px-4 sm:px-6 lg:px-8">
                 <div class="place-self-center space-y-6">
