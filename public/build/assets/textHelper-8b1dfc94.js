d53d182fc2bc94f4005e86a4bb3480a415f0d2b9
Mehedi Jaman <mail4mjaman@gmail.com>
2024-01-18 14:16:19 +0600

    - Build deleted

--- public/build/assets/textHelper-8b1dfc94.js
+++
@@ -1,1 +0,0 @@
-function r(e){return e.replace(/\w\S*/g,function(t){return t.charAt(0).toUpperCase()+t.substr(1).toLowerCase()})}function a(e){return e.toLowerCase().replace(/\s+/g,"-").replace(/[^\w-]+/g,"")}export{a as g,r as t};
