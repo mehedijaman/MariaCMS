21a894f211deea16e8b8800dedb9d04f2d1c072d
Mehedi Jaman <mail4mjaman@gmail.com>
2024-01-21 15:01:36 +0600

    - Web routes splitted

--- routes/web.php
+++ routes/web.php
@@ -39,27 +39,14 @@ use Spatie\Honeypot\ProtectAgainstSpam;
 | contains the "web" middleware group. Now create something great!
 |
 */
+require __DIR__ . '/website.php';
 
-/** Website Contact Routes */
-Route::get('contact', [WebsiteController::class, 'contact'])->name('contact');
-Route::post('contact', [WebsiteController::class, 'contactPost'])->name('contact.post')->middleware(ProtectAgainstSpam::class);
+/** Locale Routes */
+Route::get('/set-locale/{locale}', function ($locale) {
+    Session::put('locale', $locale);
 
-/** Website Testimonial Routes */
-Route::get('testimonials', [WebsiteController::class, 'testimonials'])->name('testimonials.view');
-
-/** Website Gallery Routes */
-Route::get('gallery/{slug?}', [WebsiteController::class, 'gallery'])->name('gallery');
-
-/** Website Blog Routes */
-Route::get('blog/{slug?}', [WebsiteController::class, 'blogPosts'])->name('blog.posts');
-Route::get('blog/category/{slug?}', [WebsiteController::class, 'blogCategoryPosts'])->name('blog.category.posts');
-
-/** Website Product Routes */
-Route::get('products/{slug?}', [WebsiteController::class, 'products'])->name('products');
-Route::get('products/category/{slug?}', [WebsiteController::class, 'categoryProducts'])->name('category.products');
-
-/** Website Index Routes */
-Route::get('/{slug?}', [WebsiteController::class, 'index'])->name('index');
+    return back();
+})->name('set-locale');
 
 /** Locale Routes */
 Route::get('/set-locale/{locale}', function ($locale) {
