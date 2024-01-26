53f043e933a00d0188b034aa432df2f65709949c
Mehedi Jaman <mail4mjaman@gmail.com>
2024-01-26 10:32:51 +0600

    - Updated

--- resources/js/Pages/Website/Partials/Home.vue
+++ resources/js/Pages/Website/Partials/Home.vue
@@ -5,7 +5,7 @@ const props = defineProps({
 </script>
 <template>
     <section class="bg-white shadow-lg dark:bg-gray-900 w-full mx-auto max-w-screen-xl px-4 sm:py-8 lg:px-6 my-4">
-        <img :src="props.homepage.media[0].original_url" class="shadow-lg items-center mb-4 w-full" alt="Page Image">
+        <img v-if="props.homepage.media[0]" :src="props.homepage.media[0].original_url" class="shadow-lg items-center mb-4 w-full" alt="Page Image">
         <span v-html="props.homepage.content"></span>
     </section>
 </template>
