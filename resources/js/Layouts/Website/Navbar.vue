ced7d508e4d4edb6068402efd641661a198d5044
Mehedi Jaman <mail4mjaman@gmail.com>
2024-02-04 01:23:02 +0600

    - Navbar modified - still ongoing

--- resources/js/Layouts/Website/Navbar.vue
+++ resources/js/Layouts/Website/Navbar.vue
@@ -10,8 +10,8 @@ import { XMarkIcon } from "@heroicons/vue/24/outline";
 import { initFlowbite } from 'flowbite';
 import { usePage } from "@inertiajs/vue3";
 
-const backgroundColor = usePage().props.app.setting.navbar_background_color?usePage().props.app.setting.navbar_background_color : 'white';
-const textColor = usePage().props.app.setting.navbar_text_color?usePage().props.app.setting.navbar_text_color : 'black';
+const backgroundColor = usePage().props.app.setting.navbar_background_color ? usePage().props.app.setting.navbar_background_color : 'white';
+const textColor = usePage().props.app.setting.navbar_text_color ? usePage().props.app.setting.navbar_text_color : 'black';
 
 
 const data = reactive({
@@ -39,9 +39,43 @@ onMounted(() => {
 });
 </script>
 <template>
+    <!-- <nav class="bg-white dark:bg-gray-900 fixed w-full z-20 start-0 border-b border-gray-200 dark:border-gray-600">
+  <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+  <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
+      <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo">
+      <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+  </a>
+  <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+      <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Get started</button>
+      <button data-collapse-toggle="navbar-sticky" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
+        <span class="sr-only">Open main menu</span>
+        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+        </svg>
+    </button>
+  </div>
+  <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
+    <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+      <li>
+        <a href="#" class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page">Home</a>
+      </li>
+      <li>
+        <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
+      </li>
+      <li>
+        <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</a>
+      </li>
+      <li>
+        <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
+      </li>
+    </ul>
+  </div>
+  </div>
+</nav> -->
+
     <header :style="{ backgroundColor: backgroundColor, color: textColor }" :class="data.fixed || data.isOpen
-            ? 'dark:bg-slate-900/70 backdrop-blur-lg border-b border-slate-300/50 dark:border-slate-700/50 top-0'
-            : 'dark:bg-slate-900/70 backdrop-blur-lg border border-slate-300/50 dark:border-slate-700/50'
+        ? 'dark:bg-slate-900/70 backdrop-blur-lg border-b border-slate-300/50 dark:border-slate-700/50 top-0'
+        : 'dark:bg-slate-900/70 backdrop-blur-lg border border-slate-300/50 dark:border-slate-700/50'
         " class="w-full fixed text-slate-600  dark:text-slate-200 z-50">
         <div
             class="flex flex-col max-w-7xl px-4 mx-auto sm:items-center sm:justify-between sm:flex-row sm:px-6 lg:px-6 py-2">
@@ -70,10 +104,53 @@ onMounted(() => {
                 <NavbarLink v-bind:class="route().current('index') ? 'font-bold text-primary' : ''
                     " :href="route('index')" :label="lang().label.home" />
 
-                <a v-for="item in primaryMenu?.items" v-bind:class="route().current(item.url) ? 'font-bold text-primary' : ''
-                    " :href="item.url" :target="item.target">
-                    {{ item.name }}
-                </a>
+                <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar"
+                    class="flex items-center justify-between w-full py-2 px-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-gray-400 dark:hover:text-white dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent">
+                    Products
+                    <svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                        viewBox="0 0 10 6">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="m1 1 4 4 4-4" />
+                    </svg>
+                </button>
+                <!-- Dropdown menu -->
+                <div id="dropdownNavbar"
+                    class="z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
+                    <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownLargeButton">
+                        <li>
+                            <a href="#"
+                                class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
+                        </li>
+                    </ul>
+                </div>
+
+                <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar1"
+                    class="flex items-center justify-between w-full py-2 px-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-gray-400 dark:hover:text-white dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent">
+                    Applications
+                    <svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                        viewBox="0 0 10 6">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="m1 1 4 4 4-4" />
+                    </svg>
+                </button>
+                <!-- Dropdown menu -->
+                <div id="dropdownNavbar1"
+                    class="z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
+                    <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownLargeButton">
+                        <li>
+                            <a href="#"
+                                class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
+                        </li>
+                    </ul>
+                </div>
+
+                <!-- <li>
+                    <a v-for="item in primaryMenu?.items" v-bind:class="route().current(item.url) ? 'font-bold text-primary' : ''
+                        " :href="item.url" :target="item.target">
+                        {{ item.name }}
+                    </a>
+                </li> -->
+
 
                 <NavbarLink v-bind:class="route().current('blog.posts') ? 'font-bold text-primary' : ''
                     " :href="route('blog.posts')" :label="lang().label.blog" />
@@ -88,5 +165,4 @@ onMounted(() => {
             </nav>
         </div>
     </header>
-
 </template>
--- resources/js/Pages/Website/Product/ProductDetails.vue
+++ resources/js/Pages/Website/Product/ProductDetails.vue
@@ -10,6 +10,117 @@ const activeImageURL = ref(props.product.media[0].original_url);
 </script>
 <template>
     <WebsiteLayout :title="props.title">
+        <div class="pb-10 bg-white">
+
+            <div class="bg-gray-100 h-16"></div>
+            <div class="lg:grid grid-cols-4 gap-6 px-4 py-8">
+                <div class="">
+                    <div class="border p-2 rounded-lg">
+                        <img class="h-full w-full max-w-full object-cover" :src="activeImageURL" alt="" />
+                    </div>
+                    <div class="flex gap-2 pt-4">
+                        <button @click="activeImageURL = image.original_url" v-for="image in props.product.media"
+                            :key="index" type="button"
+                            class="flex-0 aspect-square mb-3 h-20 overflow-hidden rounded-lg border-2 border-gray-900 text-center">
+                            <img class="w-16 md:w-20 h-auto cursor-pointer border-2 rounded-md  p-2" :src="image.original_url" alt="" />
+                        </button>
+                    </div>
+                </div>
+                <div class="col-span-2 ">
+                    <h1 class="text-xl py-3 font-bold text-gray-900">{{ props.product.name }}</h1>
+                    <div class="bg-gray-100  pt-4 pb-6 rounded-md px-4">
+                        <div class="flex justify-between py-3">
+                            <p class="flex gap-2 font-bold text-lg text-gray-800">Reference For Price<svg
+                                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                    stroke="currentColor" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z" />
+                                </svg>
+                            </p>
+                            <div class="">
+                                <button class="flex gap-2 font-semibold text-blue-700 hover:text-green-500">
+                                    Get Latest Price
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                        stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+                                    </svg>
+                                </button>
+                            </div>
+                        </div>
+                        <div class="md:flex gap-6 bg-white rounded-md py-4 px-2 font-bold text-red-600 md:text-xl">
+                            <h2 class="">US $1200.00-1399.00 <sub class="text-gray-600 font-semibold">/Ton</sub></h2>
+                            <h2 class="border-l pl-8"><sub class="text-gray-900 font-semibold">1 Ton<span
+                                        class="text-gray-600 font-semibold">(Min.Order)</span></sub></h2>
+                        </div>
+                    </div>
+                    <div class=" shadow-md mt-5 border">
+                        <table class="w-full text-md text-left ">
+                            <tbody class="">
+                                <tr>
+                                    <td class="px-4 py-2 font-bold text-gray-400">Grade:</td>
+                                    <td class="px-4 py-2 text-gray-900 font-semibold">1000 Series</td>
+                                </tr>
+                                <tr>
+                                    <td class="px-4 py-2 font-bold text-gray-400">Alloy or Not:</td>
+                                    <td class="px-4 py-2 text-gray-900 font-semibold">Non-alloy</td>
+                                </tr>
+                                <tr>
+                                    <td class="px-4 py-2 font-bold text-gray-400">Standard:</td>
+                                    <td class="px-4 py-2 text-gray-900 font-semibold">IS09001, RoHS, ASTM</td>
+                                </tr>
+                                <tr>
+                                    <td class="px-4 py-2 font-bold text-gray-400">Transport Package:</td>
+                                    <td class="px-4 py-2 text-gray-900 font-semibold">as Your Requirements</td>
+                                </tr>
+                                <tr>
+                                    <td class="px-4 py-2 font-bold text-gray-400">Specification</td>
+                                    <td class="px-4 py-2 text-gray-900 font-semibold">as Your Requirements</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <div class="pt-4">
+                    <div class="border px-2 py-4">
+                        <h2 class="text-gray-900 font-bold text-lg px-4 pb-4">Contact Supplier</h2>
+                        <button
+                            class="flex gap-2 mx-auto justify-center py-2 rounded-md px-8 border bg-blue-100 text-blue-800 "><svg
+                                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                stroke="currentColor" class="w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M12 20.25c4.97 0 9-3.694 9-8.25s-4.03-8.25-9-8.25S3 7.444 3 12c0 2.104.859 4.023 2.273 5.48.432.447.74 1.04.586 1.641a4.483 4.483 0 0 1-.923 1.785A5.969 5.969 0 0 0 6 21c1.282 0 2.47-.402 3.445-1.087.81.22 1.668.337 2.555.337Z" />
+                            </svg>
+                            Send Message
+                        </button>
+                        <!-- <a href="#" class="flex gap-2 mx-auto justify-center py-4  text-gray-900 font-semibold ">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                stroke="currentColor" class="w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
+                            </svg>
+                            Inquiry Basket
+                        </a> -->
+                    </div>
+                    <div class="border px-4 py-4 bg-gray-50 mt-4">
+                        <div class="flex justify-between">
+                            <img class="w-20" src="" alt="">
+                            <h6>ANJIN Metal Industries</h6>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Description -->
+            <div class="p-8">
+                <h2 class="text-gray-900 font-bold text-xl pb-4 border-b-2">Description</h2>
+                <p class="text-gray-600 p-5 leading-7 text-justify">
+                    <span v-html="props.product.description"></span>
+                </p>
+
+            </div>
+        </div>
+
+
         <section class="py-12 sm:py-16">
             <div class="container mx-auto px-4">
                 <nav class="flex">
@@ -70,41 +181,7 @@ const activeImageURL = ref(props.product.media[0].original_url);
                     <div class="lg:col-span-2 lg:row-span-2 lg:row-end-2">
                         <h1 class="sm: text-2xl font-bold text-gray-900 sm:text-3xl">{{ product.title }}</h1>
 
-                        <!-- <div class="mt-5 flex items-center">
-                            <div class="flex items-center">
-                                <svg class="block h-4 w-4 align-middle text-yellow-500" xmlns="http://www.w3.org/2000/svg"
-                                    viewBox="0 0 20 20" fill="currentColor">
-                                    <path
-                                        d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
-                                        class=""></path>
-                                </svg>
-                                <svg class="block h-4 w-4 align-middle text-yellow-500" xmlns="http://www.w3.org/2000/svg"
-                                    viewBox="0 0 20 20" fill="currentColor">
-                                    <path
-                                        d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
-                                        class=""></path>
-                                </svg>
-                                <svg class="block h-4 w-4 align-middle text-yellow-500" xmlns="http://www.w3.org/2000/svg"
-                                    viewBox="0 0 20 20" fill="currentColor">
-                                    <path
-                                        d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
-                                        class=""></path>
-                                </svg>
-                                <svg class="block h-4 w-4 align-middle text-yellow-500" xmlns="http://www.w3.org/2000/svg"
-                                    viewBox="0 0 20 20" fill="currentColor">
-                                    <path
-                                        d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
-                                        class=""></path>
-                                </svg>
-                                <svg class="block h-4 w-4 align-middle text-yellow-500" xmlns="http://www.w3.org/2000/svg"
-                                    viewBox="0 0 20 20" fill="currentColor">
-                                    <path
-                                        d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
-                                        class=""></path>
-                                </svg>
-                            </div>
-                            <p class="ml-2 text-sm font-medium text-gray-500">{{ product.rating }}</p>
-                        </div> -->
+
 
                         <h2 class="mt-8 text-base text-gray-900">Category</h2>
                         <div class="mt-3 flex select-none flex-wrap items-center gap-1">
@@ -118,56 +195,7 @@ const activeImageURL = ref(props.product.media[0].original_url);
                             </label>
                         </div>
 
-                        <!-- <h2 class="mt-8 text-base text-gray-900">Brand</h2>
-                <div class="mt-3 flex select-none flex-wrap items-center gap-1">
-                <label class="">
-                    <input type="radio" name="subscription" value="4 Months" class="peer sr-only" />
-                    <p class="peer-checked:bg-black peer-checked:text-white rounded-lg border border-black px-6 py-2 font-bold">{{ product.brand }}</p>
-                    <span class="mt-1 block text-center text-xs">$80/mo</span>
-                </label>
-                </div> -->
 
-                        <!-- <div
-                            class="mt-10 flex flex-col items-center justify-between space-y-4 border-t border-b py-4 sm:flex-row sm:space-y-0">
-                            <div class="flex items-end">
-                                <h1 class="text-3xl font-bold">${{ productStore.product.price }}</h1>
-                                <span class="text-base">/month</span>
-                            </div>
-
-                            <button @click="cartStore.add(productStore.product)" type="button"
-                                class="inline-flex items-center justify-center rounded-md border-2 border-transparent bg-gray-900 bg-none px-12 py-3 text-center text-base font-bold text-white transition-all duration-200 ease-in-out focus:shadow hover:bg-gray-800">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="shrink-0 mr-3 h-5 w-5" fill="none"
-                                    viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                                    <path stroke-linecap="round" stroke-linejoin="round"
-                                        d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z" />
-                                </svg>
-                                Add to cart
-                            </button>
-                        </div> -->
-
-                        <!-- <ul class="mt-8 space-y-2">
-                            <li class="flex items-center text-left text-sm font-medium text-gray-600">
-                                <svg class="mr-2 block h-5 w-5 align-middle text-gray-500"
-                                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                    stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                        d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                                        class=""></path>
-                                </svg>
-                                Free shipping worldwide
-                            </li>
-
-                            <li class="flex items-center text-left text-sm font-medium text-gray-600">
-                                <svg class="mr-2 block h-5 w-5 align-middle text-gray-500"
-                                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                    stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                        d="M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3-3H6a3 3 0 00-3 3v8a3 3 0 003 3z"
-                                        class=""></path>
-                                </svg>
-                                Cancel Anytime
-                            </li>
-                        </ul> -->
                     </div>
 
                     <div class="lg:col-span-3">
@@ -176,21 +204,11 @@ const activeImageURL = ref(props.product.media[0].original_url);
                                 <a href="#" title=""
                                     class="border-b-2 border-gray-900 py-4 text-sm font-medium text-gray-900 hover:border-gray-400 hover:text-gray-800">
                                     Description </a>
-
-                                <!-- <a href="#" title="" class="inline-flex items-center border-b-2 border-transparent py-4 text-sm font-medium text-gray-600">
-                    Reviews
-                    <span class="ml-2 block rounded-full bg-gray-500 px-2 py-px text-xs font-bold text-gray-100"> 1,209 </span>
-                    </a> -->
                             </nav>
                         </div>
 
                         <div class="mt-8 flow-root sm:mt-12">
-                            <span v-html="props.product.description "></span>
-                            <!-- <h1 class="text-3xl font-bold">Delivered To Your Door</h1>
-                    <p class="mt-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia accusantium nesciunt fuga.</p>
-                    <h1 class="mt-8 text-3xl font-bold">From the Fine Farms of Brazil</h1>
-                    <p class="mt-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio numquam enim facere.</p>
-                    <p class="mt-4">Amet consectetur adipisicing elit. Optio numquam enim facere. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore rerum nostrum eius facere, ad neque.</p> -->
+                            <span v-html="props.product.description"></span>
                         </div>
                     </div>
                 </div>
