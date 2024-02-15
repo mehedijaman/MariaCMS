2634b1713a2716ad93570ed9c4bb8cca7f41d5fd
Mehedi Jaman <mail4mjaman@gmail.com>
2024-02-15 16:34:26 +0600

    - Hero and CTA modified

--- resources/js/Pages/Website/Partials/CTA.vue
+++ resources/js/Pages/Website/Partials/CTA.vue
@@ -7,18 +7,18 @@ const props = defineProps({
     <div class="bg-white dark:bg-gray-900">
         <section class="bg-white dark:bg-gray-900">
             <div class="gap-8 items-center p-4 mx-auto max-w-screen-xl xl:gap-12 md:grid md:grid-cols-2 sm:py-8 lg:px-6">
-                <img class="w-full" :src="props.cta.full_path_image" alt="dashboard image">
+                <img class="w-full" :src="props.cta?props.cta.full_path_image:'https://picsum.photos/seed/picsum/580/520'" alt="CTA image">
 
                 <div class="mt-4 md:mt-0">
                     <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
-                        {{ props.cta.title }}
+                        {{ props.cta?.title }}
                     </h2>
                     <p class="mb-6 font-light text-gray-500 md:text-lg dark:text-gray-400">
-                        {{ props.cta.description }}
+                        {{ props.cta?.description }}
                     </p>
-                    <a v-if="props.cta.button_text" :href="props.cta.button_url" :target="props.cta.button_target" type="button"
+                    <a v-if="props.cta?.button_text" :href="props.cta.button_url" :target="props.cta.button_target" type="button"
                         class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
-                        {{ props.cta.button_text }}
+                        {{ props.cta?.button_text }}
                     </a>
                 </div>
             </div>
--- resources/js/Pages/Website/Partials/Hero.vue
+++ resources/js/Pages/Website/Partials/Hero.vue
@@ -16,17 +16,17 @@ const props = defineProps({
                 <div class="col-span-1 md:col-span-2">
                     <h3
                         class="max-w-2xl mb-4 text-2xl font-extrabold tracking-tight leading-none md:text-4xl xl:text-4xl dark:text-white">
-                        {{ props.hero.title }}
+                        {{ props.hero?.title }}
                     </h3>
                     <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8  dark:text-gray-400">
-                        {{ props.hero.description }}
+                        {{ props.hero?.description }}
                     </p>
-                    <a v-if="props.hero.button_text" :href="props.hero.button_url" :target="props.hero.button_target" type="button"
+                    <a v-if="props.hero?.button_text" :href="props.hero?.button_url" :target="props.hero?.button_target" type="button"
                         class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
-                        {{ props.hero.button_text }}
+                        {{ props.hero?.button_text }}
                     </a>
                 </div>
-                <img :src="props.hero.full_path_image" class="col-span-1 rounded-sm shadow-lg w-auto h-auto" alt="Hero Image">
+                <img :src="props.hero?props.hero.full_path_image:'https://picsum.photos/seed/picsum/580/520'" class="col-span-1 rounded-sm shadow-lg w-auto h-auto" alt="Hero Image">
 
             </div>
         </div>
