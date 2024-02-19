42506a0f8ed7d848998c5f6940019ddc2b9f7bd9
Mehedi Jaman <mail4mjaman@gmail.com>
2024-02-19 06:45:30 +0600

    Page Fixed

--- resources/js/Pages/Website/Page.vue
+++ resources/js/Pages/Website/Page.vue
@@ -12,7 +12,7 @@ const props = defineProps({
                 class="bg-white dark:bg-gray-900 w-full mx-auto max-w-screen-xl px-4 sm:py-8 lg:px-6 my-4">
                 <div class="place-self-center space-y-6 mt-8 p-4 max-w-7xl bg-white dark:bg-slate-800">
                     <h1 class="text-2xl pb-4 font-semibold">{{ props.data.page.name }}</h1>
-                    <img v-if="props.data.page.media" :src="props.data.page.media ? props.data.page.media[0].original_url : ''"
+                    <img v-if="props.data.page.media[0]" :src="props.data.page.media[0] ? props.data.page.media[0].original_url : ''"
                         class="shadow-lg items-center mb-4 w-1/2 h-72 mx-auto" alt="Page Image">
                     <span v-html="props.data.page.content" class="text-left w-full"></span>
                 </div>
