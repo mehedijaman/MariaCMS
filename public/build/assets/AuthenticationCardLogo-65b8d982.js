3b4b47a1730e09cb50c6a25ae5a81003c20bd915
Mehedi Jaman <mail4mjaman@gmail.com>
2024-03-07 18:06:03 +0600

    - Updated for table

---
+++ public/build/assets/AuthenticationCardLogo-65b8d982.js
@@ -0,0 +1,1 @@
+import{o as a,c as o,w as t,b as e,u as r,s}from"./app-753cbe3d.js";import{A as c}from"./ApplicationLogo-29f09a28.js";const f={__name:"AuthenticationCardLogo",setup(n){return(_,i)=>(a(),o(r(s),{href:"/"},{default:t(()=>[e(c,{class:"h-auto w-8"})]),_:1}))}};export{f as _};
